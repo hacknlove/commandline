51bb22478a7201fbf7f8cf6797227d1d6e75e292
antonio <root@hacknlove.org>
2016-08-18 10:34:12 +0200

    clean some console.log

--- client/paths.js
+++ client/paths.js
@@ -8,12 +8,10 @@ FlowRouter.route('/:path*', {
     subscription = Meteor.subscribe('path', context.path)
 
     computation = Tracker.autorun(function (computation) {
-      console.log('do')
       console.log(subscription.ready())
       if (!subscription.ready()) {
         return
       }
-      console.log('re')
       var text = paths.findOne({_id: context.path}, {fields: {text: 1}})
       if (text.text) {
         lines.insert({text: text.text})
